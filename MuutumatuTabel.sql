b8c3b61c14fe01eaddaa7f2f4c72916603c2f0e5
Märten <marten.mandla@gmail.com>
2017-03-28 15:18:37 +0300

    Konfiguratsioon

---
+++ MuutumatuTabel.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Muutumatu (
+ID int PRIMARY KEY NOT NULL IDENTITY(1,1),
+FirstName varchar(255) NOT NULL,
+LastName varchar(255) NOT NULL,
+DOB date NOT NULL,
+Email varchar(255) NOT NULL,
+PSW varchar(255) NOT NULL,
+Citizenship varchar(255) NOT NULL,
+Gender varchar(6) NOT NULL
+);
